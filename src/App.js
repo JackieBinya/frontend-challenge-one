e18216cf6e49825c40992a212f080edfa98d942e
JackieBinya <jacquelinebinya@gmail.com>
2021-05-24 12:20:14 +0200

    Adds a header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.scss';
+import { Header } from './components/Header';
 
-export const App = () => (
-  <>
-    <h1>Todos DND</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </p>
-  </>
-);
+export const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Header = () => (
+  <header>
+    <h1>Todos DnD App</h1>
+  </header>
+);
